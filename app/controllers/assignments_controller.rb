38a6a3b8469e5d2e8a9bef505b897fe8ba9a61f1
johnson-rl <johnson.rl@gmail.com>
2017-02-09 12:02:39 -0800

    Assignment Index Controller and View

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,31 @@
 class AssignmentsController < ApplicationController
+
+  def index
+    @course = Course.find_by_id(params[:course_id])
+    @assignments = @course.assignments
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
