fafcee25d047d7fb7edd07acfb0fb7ab0e9c6441
mblair415 <mblair415@gmail.com>
2017-02-08 23:00:12 -0800

    2 examples of styles for starter linking for show students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,11 @@
 class StudentsController < ApplicationController
   def index
-
+    @course = Course.find_by_id(params:course_id)
+    @students = Student.all
   end
 
   def show
-
+    @course = Course.find_by_id(params:course_id)
+    @student = Student.find_by_id(params:id)
   end
 end
