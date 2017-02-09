6404838cedfc32799a4560e5a319d43cf42ae4b9
vedelopment <vedelopment@gmail.com>
2017-02-08 20:53:18 -0800

    create sprint one routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+
+  # Courses
+  get '/courses/:id' => 'courses#show', as: 'course'
+
+  # Assignments
+  get '/courses/:course_id/assignments' => 'assignments#index', as: 'assignments'
+  get '/courses/:course_id/assignments/:id' => 'assignments#show', as: 'assignment'
+
+  # Students
+  get '/courses/:course_id/students' => 'students#index', as: 'students'
+  get '/courses/:course_id/students/:id' => 'students#show', as: 'student'
+  get '/students/new' => 'students#new', as: 'new_student'
+  post '/students' => 'students#create'
+  get '/students/:id/edit' => 'students#edit', as: 'edit_student'
+  patch '/students/:id' => 'students#update'
+
+  # Sessions
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
