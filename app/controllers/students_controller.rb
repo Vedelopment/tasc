b218d7a2c7645380f17a88d44acbd49b4130a6bc
mblair415 <mblair415@gmail.com>
2017-02-09 08:15:42 -0800

    outline for the edit ability through the controller and view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,33 @@ class StudentsController < ApplicationController
     @course = Course.find_by_id(params:course_id)
     @student = Student.find_by_id(params:id)
   end
+
+  def new
+    @course = Course.find_by_id(params:course_id)
+
+  end
+
+  def create
+    @course = Course.find_by_id(params:course_id)
+    student = Student.new(student_params)
+      if student.save
+        ** SHOVE student IN student array **
+        redirect_to student_path(@course, student)
+      else
+        flash [:error] = student.errors.full_messages.join(". ")
+        redirect_to new_student_path
+      end
+  end
+
+  def edit
+    @course = Course.find_by_id(params:course_id)
+    @student = Student.find_by_id(params:id)
+  end
+
+  private
+
+  def user_params
+    params.require(:student).permit(:first_name, :last_name, :user_name, :email, :password_digest, :slack, :github, :linkedin)
+  end
+
 end
