ea45c34aa95a405e0d08d9449c6f54c7f613d8c4
mblair415 <mblair415@gmail.com>
2017-02-09 22:51:59 -0800

    second pass at seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,37 +13,109 @@ Submission.destroy_all
 Feedback.destroy_all
 
 student_data = [{
-  first_name: "shiv",
-  last_name: "stabby",
-  user_name: "shivngiggles",
-  email: "shiv@shiv.shiv",
+  first_name: "Ivana",
+  last_name: "Tinkle",
+  user_name: "ivyT",
+  email: "ivyT@google.com",
   password_digest: "a",
-  slack: "shivngigglesonslack",
-  github: "shivngiggles",
-  linkedin: "shivngiggles"
-  }]
+  slack: "ivyT",
+  github: "ivyT",
+  linkedin: "IvyT"
+  }, {
+    first_name: "Amanda",
+    last_name: "Hugunkys",
+    user_name: "AmandaH",
+    email: "AHugunkys@google.com",
+    password_digest: "a",
+    slack: "AmandaH",
+    github: "AHugunkys",
+    linkedin: "AHugunkys"
+  }, {
+    first_name: "George",
+    last_name: "Jetson",
+    user_name: "MeetMe",
+    email: "MistaJ2@google.com",
+    password_digest: "a",
+    slack: "MistaJ2",
+    github: "GeorgeJ",
+    linkedin: "GeorgeJ"
+  }, {
+    first_name: "Dexter",
+    last_name: "Morgan",
+    user_name: "Dex",
+    email: "splatterGuy@google.com",
+    password_digest: "a",
+    slack: "Dex",
+    github: "MiamiSplatter",
+    linkedin: "DexterMorgan"
+  }, {
+    first_name: "Walter",
+    last_name: "White",
+    user_name: "TheOneWhoKnocks",
+    email: "WW@google.com",
+    password_digest: "a",
+    slack: "MisterWhite",
+    github: "WWhite",
+    linkedin: "WWhite"
+    }]
 
 course_data = [{
   name: "WDI",
   number: 34
-  }]
+  }, {
+    name: "WDI",
+    number: 35
+    }]
 
 assignment_data = [{
   title: "this is how we do it",
   instructions: "it's friday night and I feel all right.",
-  assign_date: nil,
+  assign_date: DateTime.new(2015,22,06),
   duration: 1.0,
   visible: true
+  }, {
+    title: "Putting the fun in function!",
+    instructions: "First, I need all of you to stop laughing.  Next, write a function.  Ain't that fun?",
+    assign_date: DateTime.new(2015,22,06),
+    duration: 0.25,
+    visible: true
+  }, {
+    title: "The dangers of body parsing without limbering up first",
+    instructions: "Near the surface of the DOM you'll find the body.  See it?  Good.  Now, parse it.",
+    assign_date: DateTime.new(2015,22,06),
+    duration: 7.0,
+    visible: true
+  }, {
+    title: "Fear of commitment leads to lost data.",
+    instructions: "Commit every time you do a thing that works or gives you one fewer error.  Here's a checklist for you:  a) Have you commited?  b) Did you do a thing?  C)  Does it work or did it give you at least one fewer error?  orange) See step 1.",
+    assign_date: DateTime.new(2015,22,06),
+    duration: 0.5,
+    visible: true
   }]
 
 submissions_data = [{
   content: "ooga, booga",
   link: "https://www.google.com"
+  }, {
+    content: "You have to look carefully for errors.",
+    link: "http://img.memecdn.com/epic-face_o_232179.gif"
+  }, {
+    content: "Submission data, submission data.  Submission ... data.  sbmssndt, wait, I mean SUBMISSION DATA!",
+    link: "https://s-media-cache-ak0.pinimg.com/originals/48/10/7e/48107e9a87c50894b348df59fc875830.jpg"
   }]
 
 feedback_data = [{
   score: 2.4,
-  content: "I think you should focus on friendly URL."
+  content: "I think you should focus on friendly URLs."
+  }, {
+    score: 3.6,
+    content: "Did you know that the best time to start implementing friendly URLs is always?"
+  }, {
+    score: 4.5,
+    content: "I can only think of one thing worth wishing for on your birthday would be some magic way to make URLs just a little more friendly."
+  }, {
+    score: 3.0,
+    content: "It seemed clear, from the moment I saw these URLs that you were headed in the right direction.  These are some friendly URLs."
   }]
 
 Student.create(student_data)
