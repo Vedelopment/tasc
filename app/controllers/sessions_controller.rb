5974e7b87ba0ae63c1b54d8ca01855c125230a38
ricarellano <rarellanob@gmail.com>
2017-02-10 14:04:25 -0800

    student login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    student_params = params.require(:student).permit(:email, :password_digest)
+    student_params = params.require(:student).permit(:email, :password)
     @student = Student.confirm(student_params)
     if @student
       login(@student)
       flash[:notice] = "Dude!  You logged in!"
-      redirect_to course_path(@student.course_id)
+      redirect_to course_path(Course.first)
     else
       flash[:error] = "You typed your password or email wrong. Check caps lock!"
       redirect_to login_path
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,14 @@ class Student < ApplicationRecord
   has_many :courses, through: :student_courses
 
   has_many :submissions
+
+  # validates :user_name, presence: true, uniqueness: true, length: {minimum: 3}
+  # validates :email, presence: true, uniqueness: true
+  has_secure_password
+
+  def self.confirm(params)
+  @student = Student.find_by({email: params[:email]})
+  @student ? @student.authenticate(params[:password]) : false
+end
+
 end
