e411f9b5caed51538497f4e268a1a9a139716cf2
vedelopment <vedelopment@gmail.com>
2017-02-11 13:13:16 -0800

    remove assignments visible validation, replace with default of true

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -5,6 +5,5 @@ class Assignment < ApplicationRecord
 
   validates :title, presence: true
   validates :assign_date, presence: true
-  validates :visible, acceptance: true
   validates :duration, numericality: { only_integer: true }
 end
