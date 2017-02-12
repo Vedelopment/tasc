115ac8293ddcf18712778e25aa571ef8797ed3be
vedelopment <vedelopment@gmail.com>
2017-02-12 10:02:58 -0800

    validate student and teacher password must contain at least 6 characters and have one letter and number

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,6 +9,7 @@ class Student < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, :format => {:with => /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/, message: "must be at least 6 characters and include one number and one letter."}
 
   def self.confirm(params)
     @student = Student.find_by({email: params[:email]})
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,9 +2,16 @@ class Teacher < ApplicationRecord
   has_many :teacher_courses
   has_many :courses, through: :teacher_courses
 
+  has_secure_password
   validates :user_name, presence: true, uniqueness: true, length: {minimum: 3}
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, :format => {:with => /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/, message: "must be at least 6 characters and include one number and one letter."}
+
+  def self.confirm(params)
+    @student = Student.find_by({email: params[:email]})
+    @student ? @student.authenticate(params[:password]) : false
+  end
 
 end
