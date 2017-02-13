342ee1ea3070447097086d43fa72ad1097860184
johnson-rl <johnson.rl@gmail.com>
2017-02-12 16:42:52 -0800

    debugged the merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ student_data = [{
   last_name: "Tinkle",
   user_name: "ivyT",
   email: "ivyT@google.com",
-  password_digest: "aaaaaa6",
+  password: "aaaaaa6",
   slack: "ivyT",
   github: "ivyT",
   linkedin: "IvyT"
@@ -29,7 +29,7 @@ student_data = [{
     last_name: "Hugunkys",
     user_name: "AmandaH",
     email: "AHugunkys@google.com",
-    password_digest: "aaaaaa6",
+    password: "aaaaaa6",
     slack: "AmandaH",
     github: "AHugunkys",
     linkedin: "AHugunkys"
@@ -38,7 +38,7 @@ student_data = [{
     last_name: "Jetson",
     user_name: "MeetMe",
     email: "MistaJ2@google.com",
-    password_digest: "aaaaaa6",
+    password: "aaaaaa6",
     slack: "MistaJ2",
     github: "GeorgeJ",
     linkedin: "GeorgeJ"
@@ -47,7 +47,7 @@ student_data = [{
     last_name: "Morgan",
     user_name: "Dex",
     email: "splatterGuy@google.com",
-    password_digest: "aaaaaa6",
+    password: "aaaaaa6",
     slack: "Dex",
     github: "MiamiSplatter",
     linkedin: "DexterMorgan"
@@ -56,7 +56,7 @@ student_data = [{
     last_name: "D",
     user_name: "TheDD",
     email: "LD@google.com",
-    password_digest: "aaaaaa6",
+    password: "aaaaaa6",
     slack: "eldee",
     github: "eldee",
     linkedin: "eldee"
@@ -65,7 +65,7 @@ student_data = [{
       last_name: "hodor",
       user_name: "hodor",
       email: "hodor@hodor.hodor",
-      password_digest: "hodor6",
+      password: "hodor6",
       slack: "hodor",
       github: "hodor",
       linkedin: "hodor"
@@ -78,7 +78,7 @@ Student.create(student_data)
     last_name: "White",
     user_name: "TheOneWhoKnocks",
     email: "WW@google.com",
-    password_digest: "aaaaaa6",
+    password: "aaaaaa6",
     slack: "MisterWhite",
     github: "WWhite"
   }, {
@@ -86,7 +86,7 @@ Student.create(student_data)
     last_name: "Cooper",
     user_name: "HangWithMe",
     email: "Coops@google.com",
-    password_digest: "aaaaaa6",
+    password: "aaaaaa6",
     slack: "WishIWasPlayingBBall",
     github: "Cooooop"
   }, {
@@ -94,7 +94,7 @@ Student.create(student_data)
     last_name: "Todd",
     user_name: "Demon_Barber",
     email: "soilentgreen@google.com",
-    password_digest: "aaaaaa6",
+    password: "aaaaaa6",
     slack: "freeShave",
     github: "STodd"
   }]
